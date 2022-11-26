bcebff64e3457c7c315de4c2fa1fb8f5ba216254
fraile05 <leonardofrailehb@gmail.com>
2022-11-26 14:19:48 -0500

    ultimo

--- src/main/java/Ciclo4a/seguridad/Repositorios/RepositorioUsuario.java
+++ src/main/java/Ciclo4a/seguridad/Repositorios/RepositorioUsuario.java
@@ -1,8 +1,11 @@
 package Ciclo4a.seguridad.Repositorios;
 
+import org.springframework.data.mongodb.repository.Query;
 import Ciclo4a.seguridad.Modelos.Usuario;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface RepositorioUsuario extends MongoRepository<Usuario,String> {
+    @Query("{'correo': ?0}")
+    public Usuario getUserByEmail(String correo);
 }
 
